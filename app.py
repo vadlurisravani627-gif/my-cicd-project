6c87dbc42073179851ddd50a3d7438c3eb4743ed
Vadluri Sravani <vadlurisravani627@gmail.com>
2026-04-28 19:55:51 +0530

    Test CI/CD pipeline trigger

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
     <html>
         <body style="font-family: Arial; text-align: center; margin-top: 100px; background-color: #f0f8ff;">
             <h1> My CI/CD Pipeline Project</h1>
-            <p>Deployed automatically using GitHub Actions + Docker + Render!</p>
+            <p><p>Deployed automatically using GitHub Actions + Docker + Render! 🚀</p>
             <p style="color: green;">✅ Pipeline is working!</p>
         </body>
     </html>
