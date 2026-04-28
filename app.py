db402b31db679e5387a4ec443e37fb3f9e4f9930
Vadluri Sravani <vadlurisravani627@gmail.com>
2026-04-28 21:43:55 +0530

    Update to version 3.0

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def home():
     return """
     <html>
         <body style="font-family: Arial; text-align: center; margin-top: 100px; background-color: #f0f8ff;">
-            <h1>🚀 My CI/CD Pipeline Project - v2.0</h1>
+            <h1>🚀 My CI/CD Pipeline Project - v3.0</h1>
             <p>Deployed automatically using GitHub Actions + Docker + Render! 🚀</p>
             <p style="color: green;">✅ Pipeline is working!</p>
         </body>
