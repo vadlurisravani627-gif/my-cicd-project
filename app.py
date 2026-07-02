b5e54b6760b6015a2f42686c629fcb433ec5aec9
Vadluri Sravani <vadlurisravani627@gmail.com>
2026-07-02 21:39:00 +0530

    Add Student Management System

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home():
     <!DOCTYPE html>
     <html>
     <head>
-        <title>Student Management System</title>
+        <title>Student Management Systems</title>
         <style>
             * { margin: 0; padding: 0; box-sizing: border-box; }
             body {
